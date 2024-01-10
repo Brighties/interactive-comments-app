45e0784d8fdbf56e4fc15dc3883f86f65cbb8289
Bright Udoette <brightudoette77@gmail.com>
2024-01-10 19:41:07 +0100

    Created 4 components that will be integral to the app

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,10 @@ import "./App.css";
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
+import iconMinus from "../../images/icon-minus.svg";
+import iconPlus from "../../images/icon-plus.svg";
+import iconReply from "../../images/icon-reply.svg";
+
 function App() {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
@@ -33,23 +37,55 @@ function App() {
   return (
     <>
       <Header data={data} index={0} />
-      <p>I am happy coding</p>
+      <Comments data={data} index={0} />
+      <Reply />
+      <Button />
     </>
   );
 }
 
+// component 1
 const Header = ({ data, index }) => {
-  const user = data.comments[index].user;
-  const comments = data.comments[index];
+  const person = data.comments[index];
+
   return (
-    <div>
-      <div className="comment">
-        <img src={user.image.png} alt="person avatar" />
-        <h3>{user.username}</h3>
-      </div>
-      <article>{comments.content}</article>
+    <div className="header flex">
+      <img src={person.user.image.png} alt="person avatar" />
+      <h3>{person.user.username}</h3>
+      <p>{person.createdAt}</p>
     </div>
   );
 };
 
+// component 2 = comments content body
+const Comments = ({ data, index }) => {
+  const comments = data.comments[index];
+  return <p>{comments.content}</p>;
+};
+
+// component 3 = button
+
+const Button = () => {
+  return (
+    <button className="flex btn vote-btn">
+      <img src={iconMinus} alt="icon" />
+      <span>12</span>
+      <img src={iconPlus} alt="icon" />
+    </button>
+  );
+};
+
+// component 4
+const Reply = () => {
+  // used inline styling for the purpose of introducing variety in my code just for fun
+  const style = {
+    paddingInlineStart: 1 + "rem",
+  };
+  return (
+    <button className="btn reply-btn">
+      <img src={iconReply} />
+      <span style={style}>Reply</span>
+    </button>
+  );
+};
 export default App;
